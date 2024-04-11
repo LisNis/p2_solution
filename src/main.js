a3aeea145afdcd7c4ea7b0fed1317ade9a0e943f
Liza <darthchratis22@gmail.com>
2024-04-11 20:10:39 +0200

    Fixed and added css and new sites

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ const loginBtn = document.getElementById("submit-btn");
 const userName = document.getElementById("user-name");
 const groupName = document.getElementById("group-name");
 const inputPost = document.getElementById("input-post");
+const signupBtn = document.getElementById("signup-btn");
 
 loginBtn.addEventListener("click", function() {
     // if user found in json 
-    window.location.href = "post.html";
+    window.location.href = "groups.html";
 });
 
 
-
+signupBtn.addEventListener("click", function() {
+    window.location.href = "signup.html";
+});
