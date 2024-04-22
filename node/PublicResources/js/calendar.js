925b15a4dcaebf4d0c27f005e422d0a25051eb02
Liza <darthchratis22@gmail.com>
2024-04-22 10:50:14 +0200

    Fixed calendar and started with reminder

--- node/PublicResources/js/calendar.js
+++ node/PublicResources/js/calendar.js
@@ -1,113 +1,183 @@
+// der er endnu det problem, at man kan se alle events fra alle dage
+// og den gemmer i local storage...
+
 // calendar
 const header = document.querySelector('.calendar h3');
 const dates = document.querySelector('.dates');
-const navs = document.querySelectorAll('#prev, #next');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const newEventModal = document.getElementById('newEventModal');
+const backDrop = document.getElementById('modalBackDrop');
 
 // the todo list
 const inputBox = document.getElementById('todo-input');
 const listContainer = document.getElementById('list-container');
+const cancelBtn = document.getElementById('cancel-button');
+const addBtn = document.getElementById('add-button');
 
-const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
+
+let nav = 0;
+let clicked = null;
+let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
+
+
+const weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
 ];
 
-let date = new Date();
-let month = date.getMonth(); 
-let year = date.getFullYear();
+function openModal(date) {
+    clicked = date;
+    newEventModal.style.display = 'block';
+    backDrop.style.display = 'block';
+
+}
 
 function renderCalendar() {
-    const start = new Date(year, month, 1).getDay();
-    const endDate = new Date(year, month + 1, 0).getDate();
-    const end = new Date(year, month, endDate).getDay();
-    const endDatePrev = new Date(year, month, 0).getDate();
 
-    let datesHtml = '';
+    const newDate = new Date();
 
-    // Previous months dates
-    for (let i = start; i > 0; i--) {
-        datesHtml += `<li class="inactive">${endDatePrev - i + 1}</li>`;
+    if (nav !== 0) {
+        newDate.setMonth(new Date().getMonth() + nav);
     }
 
-    // Current months dates
-    for (let i = 1; i <= endDate; i++) {
-        let className =
-            i === date.getDate() &&
-            month === new Date().getMonth() &&
-            year === new Date().getFullYear()
-                ? ' class="today"'
-                : '';
+    const day = newDate.getDate();
+    const month = newDate.getMonth(); 
+    const year = newDate.getFullYear();
 
-        datesHtml += `<li${className}>${i}</li>`;
-    }
-
-    // Next months dates
-    for (let i = end + 1; i < 7; i++) {
-        datesHtml += `<li class="inactive">${i - end}</li>`;
-    }
-
-    dates.innerHTML = datesHtml;
-    header.textContent = `${months[month]} ${year}`;
-
-    navs.forEach(nav => {
-        nav.addEventListener('click', e => {
-            const btnId = e.target.id;
-    
-            if (btnId === 'prev') {
-                month = (month === 0) ? 11 : month - 1;
-                year = (month === 11) ? year - 1 : year;
-            } else {
-                month = (month === 11) ? 0 : month + 1;
-                year = (month === 0) ? year + 1 : year;
-            }
-    
-            renderCalendar();
-        });
+    // which year, which month, the first day
+    const firstDayofMonth = new Date(year, month, 1);
+    // which year, the next month, the last day of the previous month
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    // first day of the month, based on location, so month/day/year
+    const dateString = firstDayofMonth.toLocaleDateString('en-us', {
+        // Long: Monday, short: Mon, Numeric: 1
+        weekday: 'long', 
+        year: 'numeric', 
+        month: "numeric",
+        day: 'numeric'
     });
-       
+    
+    // only want to get the first element, the weekday
+    const extraDays = weekdays.indexOf(dateString.split(', ')[0]);
+    console.log(extraDays);
+
+    header.textContent = 
+    `${newDate.toLocaleDateString('en-us', { month: 'long'})} ${year}`;
+
+    dates.innerHTML = '';
+
+    for(let i = 1; i <= extraDays + daysInMonth; i++) {
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+
+        const dayString = `${month + 1} / ${i-extraDays} / ${year}`;
+
+        if (i > extraDays) {
+            daySquare.innerText = i - extraDays;
+
+            // highlight current day
+            if (i - extraDays === day && nav === 0) {
+                daySquare.id = 'currentDay';
+            }
+
+            const eventForDay = events.find(e => e.date === dayString);
+
+            if(eventForDay) {
+                const eventDiv = document.createElement('div');
+                eventDiv.classList.add('event');
+                eventDiv.innerText = eventForDay.title;
+                daySquare.appendChild(eventDiv);
+            }
+
+            daySquare.addEventListener('click', () => {
+                openModal(dayString);
+            });
+
+        } else {
+            daySquare.classList.add('extra');
+        }
+
+        dates.appendChild(daySquare);
+    }
+   
+
 }
+
+function buttons() {
+    nextBtn.addEventListener('click', () => {
+        nav++;
+        renderCalendar();
+    });
+
+    prevBtn.addEventListener('click', () => {
+        nav--;
+        renderCalendar();
+    });
+}
+
+buttons();
 renderCalendar();
 
-function addTask(){
-    if(inputBox.value === ''){
-        console.log("You need to write something");
-    } else {
+
+// the todo list
+
+addBtn.addEventListener('click', function(){
+    if(inputBox.value){
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement('span');
+        // the x symbol
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+
+        events.push({
+            date: clicked,
+            title: inputBox.value
+        });
+
+        localStorage.setItem('events', JSON.stringify(events));
+
+    
+    } else {
+        console.log("You need to write something");  
     }
     inputBox.value = '';
-    saveData();
-}
+
+})
+
+cancelBtn.addEventListener('click', function() {
+    newEventModal.style.display = 'none';
+    backDrop.style.display = 'none';
+    inputBox.value = '';
+    clicked = null;
+    renderCalendar();
+});
 
 listContainer.addEventListener('click', function(e) {
     if(e.target.tagName === 'LI'){
         e.target.classList.toggle('checked');
-        saveData();
+        deleteEvent();
 
     } else if (e.target.tagName === 'SPAN') {
         e.target.parentElement.remove();
-        saveData();
+        deleteEvent();
     }
 
 });
 
-function saveData() {
-    localStorage.setItem('data', listContainer.innerHTML);
+function deleteEvent() {
+    events = events.filter(e => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(events));
+    renderCalendar();
 }
+
 
 function displayList() {
     listContainer.innerHTML = localStorage.getItem('data');
--- node/PublicResources/js/post.js
+++ node/PublicResources/js/post.js
@@ -1,7 +1,27 @@
+
+// posting site
 const postButton = document.getElementById("post-btn");
 const inputElement = document.getElementById("input-post");
 const displayElement = document.getElementById("displayText");
 
+
+// bot send messages
+const chatInput = document.querySelector('.chat-input textarea');
+const sendTextBtn = document.querySelector('.chat-input button');
+const chatBox = document.querySelector('.chatbox');
+
+// bot open and close chat
+const botToggler = document.querySelector('.bot-toggler');
+const botCloseBtn = document.querySelector('.close-btn');
+
+// bot countdown
+const daysHtml = document.querySelector('.days');
+const hoursHtml = document.querySelector('.hours');
+const minutesHtml = document.querySelector('.minutes');
+const secondsHtml = document.querySelector('.seconds');
+
+
+//posting site
 postButton.addEventListener("click", function() {
     let userInput = inputElement.value;
     console.log("slay");
@@ -9,7 +29,7 @@ postButton.addEventListener("click", function() {
 });
 
 
-
+//sidebar
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
@@ -18,3 +38,84 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+
+//bot send messages
+let userMessage;
+
+const createTextLi = (message, className) => {
+    const textLi = document.createElement('li');
+    textLi.classList.add('chat', className);
+    let chatContent = className === 'outgoing' ? `<p>${message}</p>` : `<span class="material-smbols">:0</span><p>${message}</p>`;
+    textLi.innerHTML = chatContent;
+    return textLi;
+}
+
+const generateResponse = (incomingTextLi) => {
+    const messageElement = incomingTextLi.querySelector('p');
+
+    // check if its right formal if not, error message, css red font and chatbox
+    userMessage = chatInput.value;
+    console.log(userMessage);
+
+    // bot Countdown
+    const newTime = new Date(userMessage);
+
+    function updateCountdown() {
+        const currentTime = new Date();
+        const diff = newTime - currentTime;
+
+        const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+        const hours =  Math.floor(diff / 1000 / 60 / 60) % 24;
+        const minutes = Math.floor(diff / 1000 / 60) % 60;
+        const seconds = Math.floor(diff / 1000) % 60;
+
+        let result = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
+
+        messageElement.textContent = result;
+
+        let end = days + hours + minutes + seconds;
+
+        //console.log(result);
+
+        if (end === 0) {
+            alert("The countdown has ended, it's time for a break!")
+        }
+
+        // stop the timer when 0, stop timer when writing another one, 
+        // delete text in chatbox, after sending
+    }
+
+    setInterval(updateCountdown, 1000);
+}
+
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage) return;
+
+    // append user message to chatbox
+    chatBox.appendChild(createTextLi(userMessage, 'outgoing'));
+    chatBox.scrollTo(0, chatBox.scrollHeight);
+
+    chatInput.value = '';
+
+    setTimeout(() => {
+        const incomingTextLi = createTextLi("Writing...", 'incoming');
+        chatBox.appendChild(incomingTextLi);
+        chatBox.scrollTo(0, chatBox.scrollHeight);
+        generateResponse(incomingTextLi);
+    })
+}
+
+sendTextBtn.addEventListener('click', handleChat);
+
+
+//bot open and close chat
+botToggler.addEventListener('click', function() {
+    document.body.classList.toggle('show-bot');
+});
+
+botCloseBtn.addEventListener('click', function() {
+    document.body.classList.remove('show-bot');
+});
