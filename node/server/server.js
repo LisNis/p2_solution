b0a2966556c997f8c39d024a341e933695bc21ff
atje20 <144028043+atje20@users.noreply.github.com>
2024-05-01 12:17:29 +0200

    det her er min kommentar

--- node/server/server.js
+++ node/server/server.js
@@ -1,57 +1,27 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
-const mysql = require('mysql');
 
 const hostname = '127.0.0.1';
-const port = 3250;
-
-//const serverName = "https://cs-24-dat-2-04.p2datsw.cs.aau.dk/node0/";
-const serverName = "http://localhost:3240";
-
-const DBConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'cs-24-dat-2-04.p2datsw.cs.aau.dk',
-    password: '################',
-    database: 'staff'
-});
-    
-DBConnection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err.message);
-        process.exit(1);
-    }
-    console.log('MySQL Connected!');
-});
-
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-/*const express = require('express');
-const app = express();
 const port = 3240;
 
-app.use(express.json());
-
-const db = require('./models');
-
-// Include route files
-const postRoute = require('./routes/Posts');
-
-// Use routes
-app.use('/posts', postRoute);
-
-
-
-db.sequelize.sync().then(() => {
-    app.listen(port, function(error) {
-        if (error) {
-            console.log('Something went wrong');
-        } else {
-            console.log('Server is listening to port ' + port);
-        }
-    });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('./PublicResources/html/login.html', function(error, data) {
+    if (error) {
+        res.writeHead(404);
+        res.write('Error: File not found');
+    } else {
+        res.write(data)
+    }
+    res.end();
+  })
 });
-*/
+
+server.listen(port, hostname, (error) => {
+    if (error) {
+        console.log('Something went wrong', error);
+    } else {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    }
+});
