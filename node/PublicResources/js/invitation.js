7a868418183634c88e9413ecb216d8db53558635
atje20 <atje20@student.aau.dk>
2024-05-10 10:57:57 +0200

    Creates invitation bars based on users.json Still needs to be able to accept and decline

--- node/PublicResources/js/invitation.js
+++ node/PublicResources/js/invitation.js
@@ -1,22 +1,40 @@
 const invitationsContainer = document.querySelector('.invitation');
-const addInvitationButton = document.getElementById('addInvitationButton');
 
 //function to add invitations to the invitation page
-function addInvitation() {
-    const newInvitation = document.createElement('div');
-    newInvitation.classList.add('invitation-bar');
+function generateInvitationsForUser(username) {
+    fetch('../users.json')
+        .then(response => response.json())
+        .then(users => {
+            const invitationsContainer = document.querySelector('.invitation');
+            const user = users.find(user => user.username === username);
 
-    newInvitation.innerHTML = `
-        <p>You have been invited to group: "Insert name"</p>
-        <button>Accept</button>
-        <button>Decline</button>
-    `;
+            if (user) {
+                const invitations = user.invitations;
 
-    newInvitation.querySelector('button').addEventListener('click', () => {
-        // Do something when the button is clicked
-    });
+                invitations.forEach(invitation => {
+                    const newInvitation = document.createElement('div');
+                    newInvitation.classList.add('invitation-bar');
 
-    invitationsContainer.appendChild(newInvitation);
+                    newInvitation.innerHTML = `
+                        <p>You have been invited to group: "${invitation}"</p>
+                        <button>Accept</button>
+                        <button>Decline</button>
+                    `;
+
+                    newInvitation.querySelector('button').addEventListener('click', () => {
+                        // Do something when the button is clicked
+                    });
+
+                    invitationsContainer.appendChild(newInvitation);
+                });
+            } else {
+                console.error('User not found:', username);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 }
 
-addInvitationButton.addEventListener('click', addInvitation);
+const loggedInUser = "Alisanders";
+generateInvitationsForUser(loggedInUser);
