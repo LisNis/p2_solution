0e942ea5189cb293c9203eca715622deea0a9301
anisaishak26 <dp48nq@student.aau.dk>
2024-05-10 13:02:01 +0200

    små ændringer2

---
+++ node/PublicResources/js/create.js
@@ -0,0 +1,23 @@
+//const canvas =; 
+//const
+//let elem = document.getElementById(string);
+//moderne verstion document.querySelector(selektorString);
+//document.qerySelectorAll(selektorString);
+//addEventListener
+
+//const createBtn = document.getElementById
+
+
+
+//function navigateToAnotherPage (){
+//window.location.href = create.html;}
+
+//document.getElementsByClassName('newGroup').onclick = navigateToAnotherPage;
+
+
+//const newGroup = document.getElementById("newGroup");
+
+      //  newGroup.addEventListener("click", function() {
+        // if user found in json 
+      //  window.location.href = "/html/groups.html";
+      //  });
